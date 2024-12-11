132ced56e311ae3f72b245bf777b4cac5ad3fc2d
IlyaGrebenschikov <ilushagr22@gmail.com>
2024-12-11 18:26:19 +0400

    add(database(PostgreSQL) connection, env_settings, .env_example)

--- Makefile
+++ Makefile
@@ -2,13 +2,15 @@ DC=docker compose
 DC_FILE=app.yaml
 DC_DIR=pushd docker_compose
 
+ENV_FILE=--env-file ../.env
+
 .PHONY: build
 build-app:
 	$(DC_DIR) && $(DC) -f $(DC_FILE) build
 
 .PHONY: up
 up-app:
-	$(DC_DIR) && $(DC) -f $(DC_FILE) up
+	$(DC_DIR) && $(DC) $(ENV_FILE) -f $(DC_FILE) up
 
 secret-key:
 	python -c 'from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())'
--- src/config/settings.py
+++ src/config/settings.py
@@ -12,6 +12,18 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+from .env_settings import (
+    get_app_settings,
+    get_db_settings,
+    get_env_settings
+    )
+
+# Local settings from .env
+env_settings = get_env_settings('.env')
+app_settings = get_app_settings()
+db_settings = get_db_settings()
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +32,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p^ppluegaqjkqjvc)vaed47l7xx&s!g!$-fjrfdim+65oye7d*'
+SECRET_KEY = app_settings.secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = app_settings.debug
 
 ALLOWED_HOSTS = []
 
@@ -74,8 +86,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-    }
+    'default': db_settings.get_config
 }
 
 
